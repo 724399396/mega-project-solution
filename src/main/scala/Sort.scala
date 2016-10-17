572b4e381f845b27e65d25d58b9815ce22f2eb43
liwei <liwei72439936@gmail.com>
2016-10-17 21:04:42 +0800

    finish sort

--- src/main/scala/Sort.scala
+++ src/main/scala/Sort.scala
@@ -1,5 +1,5 @@
 object Sort extends App {
-  def mergeSort[A <: Ordered[A]](list: List[A]): List[A] = {
+  def mergeSort[A](list: List[A])(implicit ev: A => Int): List[A] = {
     def merge(a: List[A], b: List[A]): List[A] = {
       (a,b) match {
         case (List(), _) => b
@@ -21,4 +21,33 @@ object Sort extends App {
     }
 
   }
+
+  def bubbleSort[A <% Ordered[A]](list: List[A]): List[A] = {
+    import scala.collection.mutable.MutableList
+    val mutableList = list.foldLeft(MutableList[A]()) {
+      case (acc,x) => acc += x
+    }
+
+    for {
+      i <- 0 until list.size
+    } {
+      var min = i
+      for {        
+        j <- i until list.size
+      } {
+        if (mutableList(j) < mutableList(min))
+          min = j
+      }
+      val tmp = mutableList(i)
+      mutableList(i) = mutableList(min)
+      mutableList(min) = tmp
+    }
+
+    mutableList.foldRight(List[A]()) {
+      case (x,acc) => x :: acc
+    }
+  }
+
+  println(mergeSort(List(5,12,2,1,3,42,4,5)))
+  println(bubbleSort(List(5,12,2,1,3,42,4,5)))
 }
