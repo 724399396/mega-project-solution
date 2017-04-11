9436d69300a2a2bbb7acb699d2a6a4e69d025e69
liww.li <liww.li@qunar.com>
2017-04-11 13:16:29 +0800

    calcualtor work version

--- calculator.hs
+++ calculator.hs
@@ -22,8 +22,8 @@ tok (')':xs) = Close : tok xs
 tok (c : xs) = tok' c xs [] where
   tok' ' ' xs     acc = Word (reverse acc) : tok xs
   tok' ')' xs     acc = Word (reverse acc) : Close : tok xs
-  tok'   c  []     acc = [Word (reverse $ c : acc)]
-  tok'   c  (x:xs) acc = tok' x xs (c:acc)
+  tok'  c  []     acc = [Word (reverse $ c : acc)]
+  tok'  c  (x:xs) acc = tok' x xs (c:acc)
 
 tokenize :: String -> [Tok]
 tokenize = filter (/= Word "") . tok
@@ -52,10 +52,105 @@ newtype P a = P { runP :: [Tok] -> Int -> (Maybe (a, [Tok]), Int) }
 parse :: P a -> [Tok] -> (Maybe a, Int)
 parse p tok = first (fst <$>) $ runP p tok 0
 
-instance Functor p where
+instance Functor P where
+  fmap f (P go) = P $ \toks n -> case go toks n of
+                                   (Nothing, n) -> (Nothing, n)
+                                   (Just (a, toks'), n') -> (Just (f a, toks'), n')
+                                   
+instance Applicative P where                             
+  pure a = P $ \toks n -> (Just (a, toks), n)
+  P ff <*> P xx = P $ \toks n ->
+    case ff toks n of
+      (Nothing, n) -> (Nothing, n)
+      (Just (f, toks'), n') -> case xx toks' n' of
+        (Nothing, n) -> (Nothing, n)
+        (Just (x, toks''), n'') -> (Just (f x, toks''), n'')
 
+instance Alternative P where
+  empty = P $ \_ _ -> (Nothing, 0)
+  P p1 <|> P p2 = P $ \toks n -> case p1 toks n of
+    (Nothing, _) -> p2 toks n
+    it@(Just res, n) -> it
+
+pRead :: Read a => P a
+pRead = P go where
+  go (Word x:rest) n = ((,rest) <$> readMay x, succ n)
+  go _             n = (Nothing, succ n)
+
+pSatisfy :: (String -> Bool) -> P String
+pSatisfy p = P go where
+  go (Word x:rest) n =
+    if p x then (Just (x, rest), succ n) else (Nothing, succ n)
+  go _             n = (Nothing, n)
+
+p :: String -> P String
+p s = pSatisfy (==s)
+
+pOpen :: P ()
+pOpen = P go where
+  go (Open:rest) n = (Just ((), rest), succ n)
+  go _           n = (Nothing, succ n)
+
+pClose :: P ()
+pClose = P go where
+  go (Close:rest) n = (Just ((), rest), succ n)
+  go _            n = (Nothing, succ n)
+
+pParens :: P a -> P a
+pParens inner = pOpen *> inner <* pClose
+
+oneOf :: [P a] -> P a
+oneOf = foldr (<|>) empty
+
+e :: P Expr
+e = Fix <$> oneOf
+  [ pParens sexpr
+  , Lit <$> pRead
+  , Pi <$ p "pi"
+  , Ee <$ p "e"
+  ]
+
+sexpr = oneOf
+  [ p "+"    *> (Add  <$> e <*> e)
+  , p "-"    *> (Sub  <$> e <*> e)
+  , p "*"    *> (Mult <$> e <*> e)
+  , p "/"    *> (Div  <$> e <*> e)
+  , p "inv"  *> (Inv  <$> e)
+  , p "pow"  *> (Exp  <$> e)
+  , p "sin"  *> (Sin  <$> e)
+  , p "cos"  *> (Cos  <$> e)
+  , p "tan"  *> (Tan  <$> e)
+  , p "!"    *> (Fact <$> e)
+  , p "ln"   *> (Log  <$> e)
+  , p "sqrt" *> (Sqrt <$> e)
+  , p "pow"  *> (Pow  <$> e <*> e)
+  ]
+
+fact :: Double -> Double
+fact n = fromIntegral $ scanl (*) 1 [1..] !! (round n)
+
+evalF :: E Double -> Double
+evalF (Lit d     ) = d
+evalF Pi          = pi
+evalF Ee           = exp 1
+evalF (Add  e1 e2) = e1 + e2
+evalF (Sub  e1 e2) = e1 - e2
+evalF (Mult e1 e2) = e1 * e2
+evalF (Div  e1 e2) = e1 / e2
+evalF (Inv  e    ) = recip (e)
+evalF (Exp  e    ) = exp   (e)
+evalF (Sin  e    ) = sin   (e)
+evalF (Cos  e    ) = cos   (e)
+evalF (Tan  e    ) = tan   (e)
+evalF (Fact e    ) = fact  (e)
+evalF (Log  e    ) = log   (e)
+evalF (Sqrt e    ) = sqrt  (e)
+evalF (Pow  e1 e2) = e1 ** e2
+  
 main :: IO ()
 main = do
-  str <- getLine
-  return ()
+  l <- getLine
+  case parse e (tokenize l) of
+    (Nothing, n)   -> putStrLn $ "No parse (" ++ show n ++ " tokens)"
+    (Just expr, _) -> print (cata evalF expr)
 
